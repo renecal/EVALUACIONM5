dfc2b6a15515c6f1b9aa556dc91f9d09065897c2
René Calfin Olate <rene.calfin@gmail.com>
2020-08-04 14:12:22 -0400

    subiendo avance

--- src/main/java/com/prueba/controllers/RegistroAccController.java
+++ src/main/java/com/prueba/controllers/RegistroAccController.java
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.prueba.entity.RegistroAccidente;
+import com.prueba.entity.TipoAccidente;
 import com.prueba.interfaceService.IRegistroAccidenteService;
+import com.prueba.interfaceService.ITipoAccidenteService;
 
 @Controller
 @RequestMapping("/registros")
@@ -18,6 +20,8 @@ public class RegistroAccController {
 
 	@Autowired
 	private IRegistroAccidenteService rAccService; 
+	@Autowired
+	private ITipoAccidenteService tipoAccidenteService;
 
 	@GetMapping("/listar")
 	public String listarRegistroAccidente(Model model) {
@@ -27,8 +31,11 @@ public class RegistroAccController {
 	} 
 	
 	@GetMapping("/nuevo")
-	public String agregarRegistroAccidente() {
-		return "nuevo";
+	public String agregarRegistroAccidente(Model model) {
+		List<TipoAccidente> tiposAccidentes = tipoAccidenteService.listarTipoAccidente();
+		model.addAttribute("title", "Nuevo Registro de Accidente");
+		model.addAttribute("tiposAccidentes", tiposAccidentes);
+		return "nuevoregistro";
 	}
 	
 }
